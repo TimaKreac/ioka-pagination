7b11d547823097dd6b2440ce86ee34d7301ed86d
Tamerlan Telgarin <t.telgarin@qomek.kz>
2025-03-02 13:59:07 +0500

    pagination change page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import "./App.scss";
 import Pagination from "./components/Pagination";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+
   return (
     <div>
       <h1>Ioka Pagination</h1>
-      <Pagination pageSize={10} />
+      <h2>page: {currentPage}</h2>
+      <Pagination pageSize={10} page={currentPage} setPage={setCurrentPage} />
     </div>
   );
 }
--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -2,20 +2,64 @@ import styles from "./styles.module.scss";
 
 interface PaginationProps {
   pageSize: number;
+  page: number;
+  setPage: (page: number) => void;
+  jumpStep?: number;
 }
 
-export default function Pagination({ pageSize }: PaginationProps) {
+const defaultJumpStep = 2;
+
+export default function Pagination({
+  pageSize,
+  page,
+  setPage,
+  jumpStep = defaultJumpStep,
+}: PaginationProps) {
+  const goToBackwardsPage = () => {
+    setPage(page - jumpStep);
+  };
+
+  const goToForwardsPage = () => {
+    setPage(page + jumpStep);
+  };
+
+  const goToPrevPage = () => {
+    setPage(page - 1);
+  };
+
+  const goToNextPage = () => {
+    setPage(page + 1);
+  };
+
+  const selectPage = (page: number) => {
+    setPage(page);
+  };
+
+  const isBackwardsPageDisabled = page - jumpStep < 1;
+  const isPrevPageDisabled = page <= 1;
+
+  const isNextPageDisabled = page >= pageSize;
+  const isForwardsPageDisabled = page + jumpStep > pageSize;
+
   return (
     <div className={styles.pagination}>
-      <button>DoubleLeft</button>
-      <button>Left</button>
+      <button onClick={goToBackwardsPage} disabled={isBackwardsPageDisabled}>
+        DoubleLeft
+      </button>
+      <button onClick={goToPrevPage} disabled={isPrevPageDisabled}>
+        Left
+      </button>
       <div className={styles.pages}>
         {[...Array(pageSize).keys()].map((index) => (
-          <button>{index + 1}</button>
+          <button onClick={() => selectPage(index + 1)}>{index + 1}</button>
         ))}
       </div>
-      <button>Right</button>
-      <button>DoubleRight</button>
+      <button onClick={goToNextPage} disabled={isNextPageDisabled}>
+        Right
+      </button>
+      <button onClick={goToForwardsPage} disabled={isForwardsPageDisabled}>
+        DoubleRight
+      </button>
     </div>
   );
 }
